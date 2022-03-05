4bddb404263d8ea4e1fff58088f39d6d0aff7b61
Will <wilito1210@gmail.com>
2022-03-04 22:16:21 -0500

    queries and id params set

--- server.js
+++ server.js
@@ -61,6 +61,20 @@ app.get('/api/animals', (req, res) => {
     res.json(results);
   });
 
+function findById(id,animalsArray) {
+  const result = animalsArray.filter(animal => animal.id === id)[0]
+  return result
+}
+
+app.get('/api/animals/:id',(req,res) => {
+  const result = findById(req.params.id,animals)
+  if (result) {
+    res.json(result)
+  } else res.send(404)  
+  
+})
+
 app.listen(3001, ()=>{
     console.log(`API server now on port 3001`);
 })
+
